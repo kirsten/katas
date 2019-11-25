b25f01d35eb676f69e39ebbf0412252351cdced1
Kirsten Nordine <kirstennordine@gmail.com>
2019-11-24 21:38:05 -0500

    Extract num_rolls_to_score_frame from score_frames
    This was the next obvious number that varied from branch to branch.
    Copied all 4 branches of logic to make a small change at first, but need
    to revisit the condition in this new method to see whether or not they
    are all required.

--- bowling/bowling.rb
+++ bowling/bowling.rb
@@ -17,13 +17,13 @@ class Game
     return score if rolls.empty?
 
     if frame_number == 10
-      score_frames(rolls.drop(num_rolls_in_frame(rolls, frame_number)), frame_number + 0, score + rolls.take(3).sum)
+      score_frames(rolls.drop(num_rolls_in_frame(rolls, frame_number)), frame_number + 0, score + rolls.take(num_rolls_to_score_frame(rolls, frame_number)).sum)
     elsif rolls.take(1).sum == 10
-      score_frames(rolls.drop(num_rolls_in_frame(rolls, frame_number)), frame_number + 1, score + rolls.take(3).sum)
+      score_frames(rolls.drop(num_rolls_in_frame(rolls, frame_number)), frame_number + 1, score + rolls.take(num_rolls_to_score_frame(rolls, frame_number)).sum)
     elsif rolls.take(2).sum == 10
-      score_frames(rolls.drop(num_rolls_in_frame(rolls, frame_number)), frame_number + 1, score + rolls.take(3).sum)
+      score_frames(rolls.drop(num_rolls_in_frame(rolls, frame_number)), frame_number + 1, score + rolls.take(num_rolls_to_score_frame(rolls, frame_number)).sum)
     else
-      score_frames(rolls.drop(num_rolls_in_frame(rolls, frame_number)), frame_number + 1, score + rolls.take(2).sum)
+      score_frames(rolls.drop(num_rolls_in_frame(rolls, frame_number)), frame_number + 1, score + rolls.take(num_rolls_to_score_frame(rolls, frame_number)).sum)
     end
   end
 
@@ -35,6 +35,19 @@ class Game
     else
       2
     end
+  end
+
+  def num_rolls_to_score_frame(rolls, frame_number)
+    if frame_number == 10
+      3
+    elsif rolls.take(1).sum == 10
+      3
+    elsif rolls.take(2).sum == 10
+      3
+    else
+      2
+    end
+
   end
 
   attr_reader :rolls
