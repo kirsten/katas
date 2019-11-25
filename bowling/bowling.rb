9ce034adbd021cca61b71d7a6e2a08a7e3d8d3bc
Kirsten Nordine <kirstennordine@gmail.com>
2019-11-24 21:42:27 -0500

    Remove unnecessary conditions in score_frames
    Now that the variants have been extracted into helper methods with their
    own logic, none of these branches are needed anymore :)

--- bowling/bowling.rb
+++ bowling/bowling.rb
@@ -16,15 +16,11 @@ class Game
   def score_frames(rolls = [], frame_number = 1, score = 0)
     return score if rolls.empty?
 
-    if frame_number == 10
-      score_frames(rolls.drop(num_rolls_in_frame(rolls, frame_number)), frame_number + 1, score + rolls.take(num_rolls_to_score_frame(rolls, frame_number)).sum)
-    elsif rolls.take(1).sum == 10
-      score_frames(rolls.drop(num_rolls_in_frame(rolls, frame_number)), frame_number + 1, score + rolls.take(num_rolls_to_score_frame(rolls, frame_number)).sum)
-    elsif rolls.take(2).sum == 10
-      score_frames(rolls.drop(num_rolls_in_frame(rolls, frame_number)), frame_number + 1, score + rolls.take(num_rolls_to_score_frame(rolls, frame_number)).sum)
-    else
-      score_frames(rolls.drop(num_rolls_in_frame(rolls, frame_number)), frame_number + 1, score + rolls.take(num_rolls_to_score_frame(rolls, frame_number)).sum)
-    end
+    score_frames(
+      rolls.drop(num_rolls_in_frame(rolls, frame_number)),
+      frame_number + 1,
+      score + rolls.take(num_rolls_to_score_frame(rolls, frame_number)).sum
+    )
   end
 
   def num_rolls_in_frame(rolls, frame_number)
